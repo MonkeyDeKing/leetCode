3661b218a4ef85612e4042c32e7f5be4c568eaf7
Victor Young <13272436992@163.com>
2017-05-09 23:05:46 +0800

    Problem148
    https://leetcode.com/problems/sort-list/#/description

--- jLeetCode/src/main/java/me/sevenleaves/easy/p148/Problem148.java
+++ jLeetCode/src/main/java/me/sevenleaves/easy/p148/Problem148.java
@@ -3,6 +3,7 @@
  */
 package me.sevenleaves.easy.p148;
 
+
 /**
  * @author Victor Young
  * @Todo: 148. Sort List
@@ -12,20 +13,66 @@ package me.sevenleaves.easy.p148;
 public class Problem148 {
 
 	public ListNode sortList(ListNode head) {
-		ListNode idx = head;
 		
-		if (idx.next == null) {
+		return mergeSort(head);
+	}
+	
+	private ListNode mergeSort (ListNode head) {
+		if (head == null || head.next ==null)
 			return head;
-		} else {
-			idx = idx.next;
-			head.next = null;
+		
+		// use fast-slow pointer to find the medium node
+		ListNode slow = head, fast = head.next;
+		while (fast != null && fast.next != null) {
+			slow = slow.next;
+			
+			fast = fast.next;
+			if (fast.next != null) {
+				fast = fast.next;
+			} else {
+				break;
+			}
 		}
 		
-		do {
-			
-		} while (idx.next != null);
+		ListNode mid = slow.next;
+		slow.next = null;
 		
-		return null;
+		ListNode tmp1 = mergeSort(head);
+		ListNode tmp2 = mergeSort(mid);
+		
+		return merge(tmp1, tmp2);
+	}
+	
+	private ListNode merge (ListNode h1, ListNode h2) {
+		ListNode head = new ListNode(-1);
+		ListNode pre = head;
+		
+		ListNode ptr1 = h1, ptr2 = h2;
+		while (ptr1 != null && ptr2 != null) {
+			if (ptr1.val <= ptr2.val) {
+				pre.next = ptr1;
+				pre = ptr1;
+				ptr1 = ptr1.next;
+			} else {
+				pre.next = ptr2;
+				pre = ptr2;
+				ptr2 = ptr2.next;
+			}
+		}
+		
+		while (ptr1 != null) {
+			pre.next = ptr1;
+			pre = ptr1;
+			ptr1 = ptr1.next;
+		}
+		
+		while (ptr2 != null) {
+			pre.next = ptr2;
+			pre = ptr2;
+			ptr2 = ptr2.next;
+		}
+		
+		return head.next;
 	}
 	
 	
---
+++ jLeetCode/src/test/java/me/sevenleaves/easy/p148/Problem148Test.java
@@ -0,0 +1,27 @@
+/**
+ * @author Victor Young
+ */
+package me.sevenleaves.easy.p148;
+
+import junit.framework.TestCase;
+import me.sevenleaves.study.sortList.ListNode;
+import me.sevenleaves.study.sortList.MergeSort;
+
+/**
+ * @author Victor Young
+ * @Todo: 
+ *
+ */
+public class Problem148Test extends TestCase {
+
+	private Problem148 p = new Problem148();
+	private MergeSort alg = new MergeSort();
+	
+	/**
+	 * Test method for {@link me.sevenleaves.easy.p148.Problem148#sortList(me.sevenleaves.easy.p148.ListNode)}.
+	 */
+	public void testSortList() {
+		
+	}
+
+} // end of class.
