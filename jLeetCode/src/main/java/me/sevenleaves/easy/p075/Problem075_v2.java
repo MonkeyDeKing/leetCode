e21200262266b01c845f21f07b7e26fc5a1a5844
Victor Young <13272436992@163.com>
2017-05-11 16:39:45 +0800

    75 Sort Colors
    https://leetcode.com/problems/sort-colors/

---
+++ jLeetCode/src/main/java/me/sevenleaves/easy/p075/Problem075_v2.java
@@ -0,0 +1,37 @@
+/**
+ * @author Victor Young
+ */
+package me.sevenleaves.easy.p075;
+
+/**
+ * @author Victor Young
+ * @Todo: 75. Sort Colors
+ * https://leetcode.com/problems/sort-colors/#/description
+ * Medium
+ */
+public class Problem075_v2 {
+    
+	public void sortColors(int[] nums) {
+    	int idx = 0, lo = 0, hi = nums.length-1;
+    	
+    	while (idx <= hi) {
+    		if (nums[idx] == 2)
+    			swap(nums, idx, hi--);
+    		else if (nums[idx] == 0) 
+    			swap(nums, idx++, lo++);
+    		else
+    			idx++;
+    	}
+    	
+    }
+	
+	private void swap (int[] nums, int i, int j) {
+		if (i == j)
+			return;
+		
+		int tmp = nums[i];
+		nums[i] = nums[j];
+		nums[j] = tmp;
+	}
+	
+} // end of class.
