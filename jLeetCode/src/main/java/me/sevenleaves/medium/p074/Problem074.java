6ed115a95cd34e4995113b72d2ff849dadb87ccf
Victor Young <13272436992@163.com>
2017-05-22 21:13:17 +0800

    Update package 074.

--- jLeetCode/src/main/java/me/sevenleaves/medium/p074/Problem074.java
+++ jLeetCode/src/main/java/me/sevenleaves/medium/p074/Problem074.java
@@ -1,15 +1,15 @@
 /**
  * @author Victor Young
  */
-package me.sevenleaves.medium.p240;
+package me.sevenleaves.medium.p074;
 
 /**
  * @author Victor Young
- * @Todo: 240. Search a 2D Matrix II.
+ * @Todo: 74. Search a 2D Matrix.
  * Medium.
- * https://leetcode.com/problems/search-a-2d-matrix-ii/#/description.
+ * https://leetcode.com/problems/search-a-2d-matrix/.
  */
-public class Problem240 {
+public class Problem074 {
 
     public boolean searchMatrix(int[][] matrix, int target) {
     	int m = matrix.length;
--- jLeetCode/src/test/java/me/sevenleaves/medium/p074/Problem074Test.java
+++ jLeetCode/src/test/java/me/sevenleaves/medium/p074/Problem074Test.java
@@ -1,18 +1,19 @@
 /**
  * @author Victor Young
  */
-package me.sevenleaves.medium.p240;
+package me.sevenleaves.medium.p074;
 
 import junit.framework.TestCase;
+import me.sevenleaves.medium.p074.Problem074;
 
 /**
  * @author Victor Young
  * @Todo: 
  *
  */
-public class Problem240Test extends TestCase {
+public class Problem074Test extends TestCase {
 
-	private Problem240 p = new Problem240();
+	private Problem074 p = new Problem074();
 	
 	public final void testSearchMatrix01() {
 		int[][] matrix = {{1,   3,  5,  7},
